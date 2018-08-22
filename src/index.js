7f7259e3f35d3dfb0b05e0c849a950a3b48a8076
Maria Florenci cames <mariaflorencicames@Marias-MacBook-Air.local>
2018-08-21 21:40:40 -0300

    hice un comentario

--- src/index.js
+++ src/index.js
@@ -4,10 +4,4 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './js/app'
 
-$(document).ready(function(){
-    console.log("document ready");
-    function hello(){
-        console.log(hello);
-    }
-
-})
+// comentario
